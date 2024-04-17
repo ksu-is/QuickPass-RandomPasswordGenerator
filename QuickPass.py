b73a787c6a47229f7808d5bca5053e6a4469e4fb
angelaajuebor <156493280+angelaajuebor@users.noreply.github.com>
2024-04-17 11:21:47 -0400

    Update QuickPass.py
    started code off by importing string and random 
    welcome message and list of options

--- QuickPass.py
+++ QuickPass.py
@@ -1,1 +1,13 @@
+import string 
+import random 
+
+print("Welcome to Quick Pass")
+
+length = int(input("Enter desired password length: "))
+
+print('''Choose character set for password :
+          1. Numbers
+          2. Letters
+          3. Special Characters 
+          4. Exit''')
 
