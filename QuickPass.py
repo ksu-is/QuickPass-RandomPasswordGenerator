4d7cbcb4a016ea20f42abd27f2fd56e945e58d52
angelaajuebor <156493280+angelaajuebor@users.noreply.github.com>
2024-04-18 23:24:01 -0400

    Update QuickPass.py
    removed "import string" added defining numbers, letters, and punctuation

--- QuickPass.py
+++ QuickPass.py
@@ -1,13 +1,16 @@
-import string 
 import random 
 
-print("Welcome to Quick Pass")
+print("Welcome to QuickPass!")
 
 length = int(input("Enter desired password length: "))
+
+Numbers = '0123456789'
+Letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# Punctuation = '~`!@#$%^&*+=:;?
 
 print('''Choose character set for password :
           1. Numbers
           2. Letters
-          3. Special Characters 
+          3. Punctuation
           4. Exit''')
 
