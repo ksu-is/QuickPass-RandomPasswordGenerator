6c0b6d28c6417fca5e5fd1b1073f12988798c42a
angelaajuebor <156493280+angelaajuebor@users.noreply.github.com>
2024-04-20 09:17:19 -0400

    Update ReferenceCode.py
    fixed lines of code

--- ReferenceCode.py
+++ ReferenceCode.py
@@ -1,51 +1,19 @@
-import string
 import random
 
-def generate_password(length, choices):
-    characterList = ''
-    for choice in choices:
-        if choice == '1':
-            characterList += string.digits
-        elif choice == '2':
-            characterList += string.ascii_letters
-        elif choice == '3':
-            characterList += string.punctuation
-    password = [random.choice(characterList) for _ in range(length)]
-    return ''.join(password)
+print('Welcome to your Password Generator')
 
-def main():
-    print("Welcome to Pass Craft Password Generator!")
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXXYZ~`!@#$%^&*+=:;?0123456789'
 
-    while True:
-        try:
-            num_passwords = int(input("How many passwords would you like to generate? "))
-            if num_passwords <= 0:
-                print("Please enter a positive integer.")
-            else:
-                break
-        except ValueError:
-            print("Please enter a valid integer.")
+number = input('Amount of passwords to generate: ')
+number = int(number)
 
-    while True:
-        try:
-            length = int(input("Enter password length: "))
-            if length <= 0:
-                print("Password length should be greater than 0.")
-            else:
-                break
-        except ValueError:
-            print("Please enter a valid integer.")
+length = input('Input desired password length: ')
+length = int(length)
 
-    print('''Choose character set for password:
-        1. Digits
-        2. Letters
-        3. Special characters
-        4. Exit (You can choose multiple options separated by spaces)''')
+print('\nhere are your passwords: ')
 
-    choices = input("Enter your choices separated by spaces: ").split()
-  
-    for i in range(num_passwords):
-        password = generate_password(length, choices)
-        print("Generated password:", password)
-if __name__ == "__main__":
-    main()
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)
