cda166974ce3026168ca41da3aff6e925e21fe1f
angelaajuebor <156493280+angelaajuebor@users.noreply.github.com>
2024-04-20 09:16:02 -0400

    Update QuickPass.py

--- QuickPass.py
+++ QuickPass.py
@@ -1,12 +1,19 @@
 import random 
-
-print("Welcome to QuickPass!")
-
-length = int(input("Enter desired password length: "))
-
+          
 Numbers = '0123456789'
 Letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-# Punctuation = '~`!@#$%^&*+=:;?
+Punctuation = '~`!@#$%^&*+=:;?"
+
+def main(): 
+    print("Welcome to QuickPass!")
+
+    while True:
+        try:           
+            
+
+amount = int(input("Amount of passwords to generate: "))
+
+length = int(input("Enter desired password length: "))
 
 print('''Choose character set for password :
           1. Numbers
